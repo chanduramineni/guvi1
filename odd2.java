57a4fa4803c517ad500fb95f4646d8b89a68fa54
chanduramineni <41728861+chanduramineni@users.noreply.github.com>
2018-08-01 15:59:49 +0530

    Create odd2.java

---
+++ odd2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class odd2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		for(int i=a+1;i<=b;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(+i + " ");
+			}
+		}
+	}
+}

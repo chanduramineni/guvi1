9bc6196c6823f0aef097c71ed87d9a16ec03738a
chanduramineni <41728861+chanduramineni@users.noreply.github.com>
2018-08-11 12:58:53 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -9,7 +9,7 @@ class fact
 	        Scanner s=new Scanner(System.in);
           int n=s.nextInt();
           int fact=1;
-          for(int i=1;i<=1;i++)
+          for(int i=1;i<=n;i++)
           {
                 fact=fact*i;
            }

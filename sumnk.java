61d8f20bef7b62f8a943fd18bec5d7f4bc74b858
chanduramineni <41728861+chanduramineni@users.noreply.github.com>
2018-08-11 10:23:01 +0530

    Create sumnk.java

---
+++ sumnk.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class sumnk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		int sum=0;
+		for(int i=0;i<k;i++)
+		{
+			sum+=a[i];
+		}
+		
+		System.out.println(sum);
+		
+		
+
+	}
+}

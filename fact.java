3ebe9d93728b8eef93667bf18413cada16b47a74
chanduramineni <41728861+chanduramineni@users.noreply.github.com>
2018-08-11 12:54:54 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class fact
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	        Scanner s=new Svanner(System.in);
+          int n=s.nextInt();
+          int fact=1;
+          for(int i=1;i<=1;i++)
+          {
+                fact=fact*i;
+           }
+           System.out.println(fact);
+	}
+}
